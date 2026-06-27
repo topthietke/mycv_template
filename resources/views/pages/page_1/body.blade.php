70ff46bfd6f8f2aa5d4a681600c8ab158050f890
Tran Ngoc Tu <ngoctusoftware@gmail.com>
2026-06-27 18:52:54 +0700

    Fix

--- resources/views/pages/page_1/body.blade.php
+++ resources/views/pages/page_1/body.blade.php
@@ -6,7 +6,7 @@
 @if (!empty($categories))
     <div class="cv-body">
         @foreach ($categories as $item)
-            @if (in_array($item['id'], $allow_page_1))
+            {{-- @if (in_array($item['id'], $allow_page_1)) --}}
                 <div class="cv-section">
                 <div class="cv-section__header">
                     <div class="cv-section__label" id="{{ $item['id'] }}" data-code="{{ $item['code'] }}">{{ $item['name'] ?? '' }}</div>
@@ -20,14 +20,9 @@
                     @endif        
                 </div>
             </div>    
-            @endif            
+            {{-- @endif             --}}
         @endforeach
-
-
-
     </div>
 @endif
-
-
 
 <!-- /cv-body -->
--- resources/views/pages/page_2.blade.php
+++ resources/views/pages/page_2.blade.php
@@ -1,87 +1,33 @@
-<div class="cv-page" id="cv-page-2" style="margin-top: 30px;">
-    <div class="cv-body" style="padding-top:30px;">
-        <!-- KINH NGHIỆM LÀM VIỆC và Dự án thực tế -->
-        <div class="cv-section">
+<!-- BODY -->
+<?php 
+    $allow_page   = config('data.page');
+    $allow_page_2 = $allow_page['page_2'];    
+?>
+@if (!empty($categories))
+    <div class="cv-page" id="cv-page-2" style="margin-top: 30px;">
+        <!-- /cv-body -->
+        <div class="cv-body" style="padding-top:30px;">
+            @foreach ($categories as $item)
+                @if (in_array($item['id'], $allow_page_2))
+                    <div class="cv-section">
+                        <div class="cv-section__header">
+                            <div class="cv-section__label" id="{{ $item['id'] }}" data-code="{{ $item['code'] }}">
+                                {{ $item['name'] ?? '' }}
+                            </div>
+                            <div class="cv-section__line"></div>
+                        </div>
 
-            <div class="cv-section__header">
-                <div class="cv-section__label">Kinh nghiệm làm việc và Dự án thực tế</div>
-                <div class="cv-section__line"></div>
-            </div>
-
-            <div class="cv-entry">
-                <!-------------------------------  Công ty Cổ phần Công nghệ HTECOM  ---------------------------------->
-                <div class="cv-entry__row">
-                    <div class="cv-entry__school">Công ty Cổ phần Công nghệ HTECOM</div>
-                    <div class="cv-entry__school">Chuyên viên lập trình</div>
-                    <div class="cv-entry__date">07/2024 - nay</div>
-                </div>
-                <!------------------------------------------------------------------------------------------------------->
-                <ul class="cv-entry__bullets">
-                    <li><b>Công nghệ: </b> PHP (Laravel), MySQL, HTML5, CSS3, Bootstrap 5, JavaScript, jQuery,
-                        Queue, Docker</li>
-                    <li><b>Dự án phụ trách:</b>
-                        <div class="cv-entry__bullets">- Dự án tuyển sinh du học Vangunu: <a class="lnk_project"
-                                href="https://vangunu.com" target="_blank">vangunu.com</a> </div>
-                        <div class="cv-entry__bullets">- Dự án Booking Spa cho khách hàng Châu Âu: <a
-                                class="lnk_project" href="https://cicireluxe.com.au"
-                                target="_blank">cicireluxe.com.au</a> </div>
-                        <div class="cv-entry__bullets">- Dự án Hệ thống CRM tuyển sinh của ĐH Mở HCM: <a
-                                class="lnk_project" href="https://crm.elolms.edu.vn"
-                                target="_blank">crm.elolms.edu.vn</a> </div>
-                        <div class="cv-entry__bullets">- Dự án mạng xã hội thanh thiếu niên: <a class="lnk_project"
-                                href="https://vlingai.com" target="_blank">vlingai.com</a> </div>
-                        <div class="cv-entry__bullets">- Dự án Liferay (CMS Java) Đường cao tốc Việt Nam: <a
-                                class="lnk_project" href="http://portal.tctvec.vn/web/guest"
-                                target="_blank">portal.tctvec.vn</a></div>
-                    </li>
-                    <li><b>Nội dung công việc:</b>
-                        <div class="cv-entry__bullets">- Phân tích yêu cầu, thiết kế giải pháp và kiến trúc phần mềm
-                            cho dự án.</div>
-                        <div class="cv-entry__bullets">- Triển khai các chức năng chính của dự án (Backend,
-                            Frontend).</div>
-                        <div class="cv-entry__bullets">- Review code và đảm bảo chất lượng.</div>
-                        <div class="cv-entry__bullets">- Deploy ứng dụng lên server.</div>
-                        <div class="cv-entry__bullets">- Debug và khắc phục lỗi.</div>
-                        <div class="cv-entry__bullets">- Sử dụng Google Gemini, Claude AI chỉnh sửa, nâng cấp hệ
-                            thống.</div>
-                    </li>
-                </ul>
-                <!------------------------------  Tập đoàn Omigroup  -------------------------------------->
-                <div class="cv-entry__row">
-                    <div class="cv-entry__school">Tập đoàn Omigroup</div>
-                    <div class="cv-entry__school">Chuyên viên lập trình</div>
-                    <div class="cv-entry__date">06/2022 đến 06/2024</div>
-                </div>
-                <!------------------------------------------------------------------------------------------------------->
-                <ul class="cv-entry__bullets">
-                    <li><b>Công nghệ: </b> PHP (Laravel), MySQL, HTML5, CSS3, Bootstrap 5, JavaScript, jQuery,
-                        Queue, Docker</li>
-                    <li><b>Dự án phụ trách:</b>
-                        <div>- Hệ thống bán hàng dược phẩm trực tuyến: <a href="http://vangunu.com" target="_blank">Bán
-                                hàng
-                                Omipharma</a></div>
-                        <div>- Hệ thống Quản lý bán hàng nội bộ (Ominext): <a
-                                href="https://omi-pos.ominext.dev/xadmin/login">Omi-pos
-                                Ominext</a></div>
-                    </li>
-                    <li><b>Nội dung công việc:</b>
-                        <div class="cv-entry__bullets">- Phối hợp các thành viên trong nhóm, tham gia phát triển và
-                            nâng cấp hệ thống API từ phiên bản PHP thuần sang PHP Lumen.</div>
-                        <div class="cv-entry__bullets">- Tham gia nghiên cứu, thiết kế và tối ưu database: MySQL,
-                            MongoDB.</div>
-                        <div class="cv-entry__bullets">- Tham gia nghiên cứu, phát triển, tích hợp các sàn TMĐT:
-                            Shopee, Lazada về hệ thống Omipos.</div>
-                        <div class="cv-entry__bullets">- Phối hợp với nhóm Kiểm thử kiểm tra và sửa lỗi.</div>
-                        <div class="cv-entry__bullets">- Tận dụng Google Gemini, Claude AI tra cưu bug logic của hệ
-                            thống.</div>
-                    </li>
-                </ul>
-            </div>
-
-
+                        <div class="cv-entry">
+                            @if (!empty($item['contents']))
+                                @foreach ($item['contents'] as $value)
+                                    <span>{!! $value['content'] !!}</span>
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+                @endif
+            @endforeach
         </div>
-
-    </div><!-- /cv-body -->
-
-
-</div>
+        <!-- /cv-body -->
+    </div>
+@endif
